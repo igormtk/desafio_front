80d4fe3f1d92fa60a59754e684ab38be46b808e8
Igor Matsuoka <igor.mtka@gmail.com>
2022-05-31 14:11:03 -0300

    consertando detalhes

--- covid-database/src/pages/homePage/homePage.js
+++ covid-database/src/pages/homePage/homePage.js
@@ -1,53 +1,54 @@
 import React, { useContext } from "react";
 import Card from "../../components/card/card";
 import LineChart from "../../components/chart/chart"
-import Header from "../../components/header/header"
+import Header from "../../components/header/header";
 import GlobalStateContext from "../../global/globalStateContext";
-import { CardContainer, CardsContainer, ChartContainer } from "./homePageStyle";
+import UpdatedDate from "../../services/date";
+import { separator } from "../../services/separator";
+import { CardContainer, CardsContainer } from "./homePageStyle";
 
 const HomePage = () => {
 
     const { states, setters } = useContext(GlobalStateContext)
 
+    // const infoConfirmed = states.cases.cases
+    // const infoConfirmedFormated = separator(infoConfirmed)
+
     return(<div>
 
-        <Header/>
+        <Header title={"COVID-19 DATABASE"}/> 
         
         <CardsContainer>
             <CardContainer>
                 <Card 
                     title = "WORLD CASES CONFIRMED"
-                    info = {states.cases.cases.toLocaleString('en-GB')}
+                    info = {states.cases.cases}
                 />
             </CardContainer>
 
             <CardContainer>
                 <Card 
                     title = "WORLD CASES RECOVERED"
-                    info = {states.cases.recovered.toLocaleString('en-GB')}
+                    info = {states.cases.recovered}
                 />
             </CardContainer>
 
             <CardContainer>
                 <Card 
                     title = "WORLD CONFIRMED DEATHS"
-                    info = {states.cases.deaths.toLocaleString('en-GB')}
+                    info = {states.cases.deaths}
                 />
             </CardContainer>
         </CardsContainer>
 
-        <ChartContainer>
-
-            <h1>{states.selects}</h1>
-            <select value={states.selects} onChange={e=>setters.setSelectes(e.target.value)}>
-                <option>TESTE</option>
-            </select>
-            
-            <div style={{ width: 500 }}>
-                <LineChart chartData={states.userData} />
-            </div>
-
-        </ChartContainer>
+        <h1>{states.selects}</h1>
+        <select value={states.selects} onChange={e=>setters.setSelectes(e.target.value)}>
+            <option>TESTE</option>
+        </select>
+        
+        <div style={{ width: 500 }}>
+            <LineChart chartData={states.userData} />
+        </div>
 
         </div>  
     )  
