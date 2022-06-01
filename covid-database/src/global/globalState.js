5570445bb6726651245168808ab904a1f38c34ed
Igor Matsuoka <igor.mtka@gmail.com>
2022-06-01 02:57:05 -0300

    tentando utilizar funções assincronas para o chart

--- covid-database/src/global/globalState.js
+++ covid-database/src/global/globalState.js
@@ -2,14 +2,90 @@ import GlobalStateContext from "./globalStateContext";
 import React, { useState } from "react";
 import { BASE_URL } from "../constants/url";
 import { useRequestData } from "../hooks/useRequestData";
-import axios from "axios";
+import { Bar } from "react-chartjs-2"
+import { 
+    Chart as ChartJS, 
+    CategoryScale, 
+    LinearScale, 
+    BarElement, 
+    Title, 
+    Tooltip, 
+    Legend 
+} from "chart.js"
+
+ChartJS.register(
+    CategoryScale, 
+    LinearScale, 
+    BarElement, 
+    Title, 
+    Tooltip, 
+    Legend
+)
 
 const GlobalState = (props) => {
+    var Chart = require('chart.js');
 
     const cases = useRequestData([], `${BASE_URL}/all`)
     const countries = useRequestData([],`${BASE_URL}/countries`)
     const especificCountry = useRequestData([], `${BASE_URL}/countries/Brazil`)
-    const historicalData = useRequestData([], `${BASE_URL}/historical/all`)
+
+    async function getCountryInfo () {
+        const url = `${BASE_URL}/countries`
+        const response = await fetch(url)
+        const dataPoints = await response.json()
+        console.log(dataPoints)
+        return dataPoints
+    }
+
+    getCountryInfo().then(datapoints =>{
+        const country = datapoints.map(datapoint => {
+            return datapoint.country
+        })
+        const countryCases = datapoints.map(datapoint => {
+            return datapoint.cases
+        })
+
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: country,
+                datasets: [{
+                    label: '# of Votes',
+                    data: countryCases,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+
+        return myChart
+    })
+
 
     let countriesList = []
     let population = []
@@ -18,10 +94,10 @@ const GlobalState = (props) => {
         population.push(country.population)
     }
 
-    const countryPopulation = especificCountry.population
-    const confirmedCases = especificCountry.cases
-    const recoveredCases = especificCountry.recovered
-    const deathCases = especificCountry.deaths 
+    const countryPopulation = [especificCountry.population]
+    const confirmedCases = [especificCountry.cases]
+    const recoveredCases = [especificCountry.recovered]
+    const deathCases = [especificCountry.deaths] 
 
     const [chartData, setChartData] = useState({
         datasets: [],
@@ -39,7 +115,8 @@ const GlobalState = (props) => {
         countryPopulation,
         confirmedCases,
         recoveredCases,
-        deathCases
+        deathCases, 
+        chart
     }
 
     const setters = { setChartData, setChartOptions }
--- covid-database/src/pages/homePage/homePage.js
+++ covid-database/src/pages/homePage/homePage.js
@@ -7,71 +7,10 @@ import {
     CardsContainer, 
     ChartContainer 
 } from "./homePageStyle";
-import { 
-    Chart as ChartJS, 
-    CategoryScale, 
-    LinearScale, 
-    BarElement, 
-    Title, 
-    Tooltip, 
-    Legend 
-} from "chart.js"
-import { Bar } from "react-chartjs-2"
-
-ChartJS.register(
-    CategoryScale, 
-    LinearScale, 
-    BarElement, 
-    Title, 
-    Tooltip, 
-    Legend
-)
 
 const HomePage = () => {
 
-    const { states, setters } = useContext(GlobalStateContext)
-
-    useEffect(()=>{
-        setters.setChartData({
-            labels: ["population", "Confirmed Cases", "Recovered Cases", "Death Cases"],
-            datasets: [
-                {
-                    label: "Population",
-                    data: states.countryPopulation,
-                    borderColor: "rgb(53, 162, 235)",
-                    backgroundColor: "blue"
-                },
-                {
-                    label: "Confirmed cases",
-                    data: states.confirmedCases,
-                    borderColor: "rgb(53, 162, 235)",
-                    backgroundColor: "red"
-                },
-                {
-                    label: "Recovered Cases",
-                    data: states.recoveredCases,
-                    borderColor: "rgb(53, 162, 235)",
-                    backgroundColor: "green"
-                },
-                {
-                    label: "Death Cases",
-                    data: states.deathCases,
-                    borderColor: "rgb(53, 162, 235)",
-                    backgroundColor: "black"
-                },
-            ]
-        })
-        setters.setChartOptions({
-            responsive: true,
-            plugins:{
-                legend: "top"
-            },
-            title: {
-                display: true,
-                text: "Test chart"
-            },
-        })
-    }, [])
+    const { states, setters } = useContext(GlobalStateContext)    
 
     return(<div>
 
@@ -106,8 +45,8 @@ const HomePage = () => {
                 <option>TESTE</option>
             </select>
             
-            <Bar options={states.chartOptions} data={states.chartData}/>
-            
+            {states.chart}
+
         </ChartContainer>
 
         </div>  
